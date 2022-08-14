691304d80536af1e0d3175ff3d6116e967f8d94d
Valeentin87 <Valeentin87@list.ru>
2022-08-14 20:47:09 +0300

    записали код программы для задачи 19

--- Zadacha19/Program.cs
+++ Zadacha19/Program.cs
@@ -1,10 +1,12 @@
-﻿// Напиштте программу, которая принимает на вход пятизначное число и проверяет, является ли оно полиндромом
+﻿// Напишbте программу, которая принимает на вход пятизначное число и проверяет, является ли оно полиндромом
 Console.WriteLine("Введите пятизначное число");
 String N = Console.ReadLine();
 Console.WriteLine($"Введенное число {Polindrom(N)}");
 String Polindrom (String Number)
 {
     int L = Number.Length; // Узнали длину введенной строки или число символов
+    int marker = 0;
+    string Nnaooborot = "";
     if (L != 5)
     {
         string otvet = "не является пятизначным";
@@ -12,23 +14,43 @@ String Polindrom (String Number)
     }
     else
     {
-        string Nnaooborot = "";
         for (int i = L; i > 0 ; i--)
         {
-           Nnaooborot = Nnaooborot + Number[L]; 
+           Nnaooborot = Nnaooborot + Number[i-1]; 
         }
-        for (int j = 1; j <= L; j++)
+        for (int j = 1; j < L; j++)
         {
             if (N[j] != Nnaooborot[j])
             {
-                string otvet1 = "не является полиндромом";
-                return otvet1;
+                marker = 1;
+                
             }
             else
             {
-                string otvet2 = "является полиндромом";
-                return otvet2;
+                if (j == L) 
+                {
+                marker = 2;
+                }
+                else marker = 3;
+
+                
             }
         }
     }
-} 
+    if (marker == 1) 
+    {
+        string otvet1 = "не является полиндромом";
+        return otvet1;
+    } 
+    else 
+    {
+        string otvet2 = "является полиндромом";
+        return otvet2;
+    }
+
+}
+
+
+
+
+
