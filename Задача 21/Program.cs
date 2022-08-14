72711c96a0ceae45b96d17b27d5b6db10769fae7
Valeentin87 <Valeentin87@list.ru>
2022-08-14 21:43:21 +0300

    промежуточное сохранение

--- Задача 21/Program.cs
+++ Задача 21/Program.cs
@@ -13,3 +13,11 @@ for (int i = 0; i < 3; i++)
     Console.WriteLine($"Введите координату номер {(i+1)} второй точки");
     B[i] = Convert.ToDouble(Console.ReadLine());
 }
+
+double result = Prostranstvo(A[0], A[1], A[2], B[0], B[1], B[2]);
+Console.WriteLine($"Расстояние между двумя точками в пространстве равно {result}");
+double Prostranstvo(double x1, double y1, double z1, double x2, double y2, double z2);
+{
+double rasst = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
+return rasst;
+}
