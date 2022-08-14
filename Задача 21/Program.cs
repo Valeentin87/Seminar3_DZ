fad451d3248eed0219d98ea85493c870e277c94b
Valeentin87 <Valeentin87@list.ru>
2022-08-14 21:18:13 +0300

    вводим координаты обеих точек

--- Задача 21/Program.cs
+++ Задача 21/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3Д пространстве
+Console.WriteLine("Введите координаты первой точки");
+double [] A = new double[3];
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Введите координату номер {(i+1)} первой точки");
+    A[i] = Convert.ToDouble(Console.ReadLine());
+}
+Console.WriteLine("Введите координаты второй точки");
+double [] B = new double[3];
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine($"Введите координату номер {(i+1)} второй точки");
+    B[i] = Convert.ToDouble(Console.ReadLine());
+}
