ae0b16d09503f8a4781237b45b173bbc16d772de
Valeentin87 <Valeentin87@list.ru>
2022-08-16 13:21:42 +0300

    округляем результат до второй цифры после запятой

--- Задача 21/Program.cs
+++ Задача 21/Program.cs
@@ -16,8 +16,9 @@ for (int i = 0; i < 3; i++)
 
 double result = Prostranstvo(A[0], A[1], A[2], B[0], B[1], B[2]);
 Console.WriteLine($"Расстояние между двумя точками в пространстве равно {result}");
-double Prostranstvo(double x1, double y1, double z1, double x2, double y2, double z2);
+double Prostranstvo(double x1, double y1, double z1, double x2, double y2, double z2)
 {
-double rasst = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2)+Math.Pow((z2-z1),2));
-return rasst;
+double rasst = Math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1));
+double rasstOkruglennoe = Math.Round(rasst,2);
+return rasstOkruglennoe;
 }
