2ec2f15832bcc53523f027208d0480cfca73d7fb
Valeentin87 <Valeentin87@list.ru>
2022-08-16 13:57:34 +0300

    успешное решение задачи 23

--- Zadacha23/Program.cs
+++ Zadacha23/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Ниже представлена таблица кубов от 1 до {N}: {Kub(N)}");
+string Kub(int Number)
+{
+    string tablisaKubov = "";
+    for(int i = 1; i <= N; i++)
+    {
+        tablisaKubov = tablisaKubov + "\n" + Convert.ToString(Math.Pow(i,3));
+    }
+    return tablisaKubov;
+}
